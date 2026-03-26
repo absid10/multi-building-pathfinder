5a731f4de568957142b5d70e19d3b08264a5414d
Abdullah Ahmed Siddiqui <siddiquiabdullahahmed75@gmail.com>
2026-03-26 06:29:04 +0530

    Update LinkedIn contact URL

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -1,4 +1,4 @@
 export const SITE_OWNER_NAME = "Abdullah Ahmed Siddiqui";
 export const SITE_CONTACT_EMAIL = "siddiquiabdullahahmed75@gmail.com";
 export const SITE_GITHUB_URL = "https://github.com/absid10";
-export const SITE_LINKEDIN_URL = "https://www.linkedin.com/in/abdullah-ahmed-siddiqui/";
+export const SITE_LINKEDIN_URL = "https://www.linkedin.com/in/absid10/";
